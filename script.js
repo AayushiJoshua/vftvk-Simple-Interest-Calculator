ae948dd8a4ddadf414ad84446120f5bbd38eea5d
AayushiJoshua <102644892+AayushiJoshua@users.noreply.github.com>
2022-09-24 01:54:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    principal = document.getElementById("principal").value;
+    rate = parseFloat(document.getElementById("rate").value);
+    years = parseInt(document.getElementById("years").value);
+
+    // console.log(principal, rate, years);
+
+    interest = (principal*rate*years)/100;
+    // console.log(interest);
+
+    resultantYear = parseInt(new Date().getFullYear())+years;
+    // console.log(date.getFullYear());
     
+    result = document.getElementById('result');
+    output = `If you deposit ${principal}, 
+            \nat an interest rate of ${rate}.
+            \nYou will receive an amount of ${interest},
+            \n  in the year ${resultantYear}`;
+
+    result.innerHTML = output; 
+}
+
+function updateRate(){
+    rate = document.getElementById("rate").value;
+    currentRate = document.getElementById("currentRate");
+    currentRate.innerHTML = rate+"%";
 }
         
